3d8cc8667b29afc41f7051588bd12779c4f868da
fotikerkeshi <foti.kerkeshi@hotmail.com>
2024-06-26 20:53:04 +0200

    dataset utils working

--- dataset/utils/datacheck.py
+++ dataset/utils/datacheck.py
@@ -3,35 +3,30 @@ import sys
 import numpy as np
 from PIL import Image
 
-
 class DataCheck:
     def __init__(self, dataset_path) -> None:
         self.dataset_path = dataset_path
 
-    def check_RGB(self, folder="test"):
+    def check_RGB(self):
 
-        folder_path = os.path.join(self.dataset_path, folder, "rgb_data")
+        folder_path = os.path.join(self.dataset_path, "rgb_data")
         paths = sorted(os.listdir(folder_path))
 
         for path in paths:
             file_dir = os.path.join(folder_path, path)
-            img = Image.open(file_dir)
-            img = np.array(img)
+            img = np.load(file_dir)
             print(f"Image path: {file_dir}")
             print(f"Image shape: {img.shape}")
             print(f"Image max: {img.max()}")
             print(f"Image min: {img.min()}")
             print(f"Image mean: {img.mean()}")
             print(f"Image std: {img.std()}")
-            print(img)
 
             assert img.shape == (256, 256, 3), "Image shape is not (256, 256, 3)"
 
-    # test dataset has correct values of XYZ
+    def check_XYZ(self):
 
-    def check_XYZ(self, folder="test"):
-
-        folder_path = os.path.join(self.dataset_path, folder, "xyz_data")
+        folder_path = os.path.join(self.dataset_path, "xyz_data")
         paths = sorted(os.listdir(folder_path))
 
         for path in paths:
@@ -43,10 +38,8 @@ class DataCheck:
             print(f"XYZ min: {xyz.min()}")
             print(f"XYZ mean: {xyz.mean()}")
             print(f"XYZ std: {xyz.std()}")
-            print(xyz)
 
             assert xyz.shape == (256, 256, 3), "XYZ shape is not (256, 256, 3)"
-
 
 if __name__ == "__main__":
     dataset_name = sys.argv[1]
