416b2d9ab276caa10176210f43e72ff4045c8cb3
fotikerkeshi <foti.kerkeshi@hotmail.com>
2024-07-29 01:15:06 +0200

    quick fid

--- models/cyclegan_resnet/test.py
+++ models/cyclegan_resnet/test.py
@@ -1,15 +1,22 @@
+import os
+from tracemalloc import start
 import torch
 import argparse
+import numpy as np
+from cleanfid import fid
 from torch.utils.data import DataLoader
+from dotenv import load_dotenv
 
 from utils import show_xyz, show_rgb, show_diff
 from data import RGBTileDataset
 from model import GeneratorResNet
 
+load_dotenv()
+
 parser = argparse.ArgumentParser(description="pix2pix-pytorch-implementation")
 parser.add_argument("--dataset_name", default="melbourne-top")
 parser.add_argument("--threads", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--batch_size", type=int, default=2, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
 parser.add_argument("--nepochs", type=int, default=10, help="saved model epochs")
 parser.add_argument("--n_residual_blocks", type=int, default=9, help="number of residual blocks in generator")
 parser.add_argument("--img_height", type=int, default=256, help="size of image height")
@@ -33,24 +40,47 @@ model_g.load_state_dict(torch.load(model_path))
 test_set = RGBTileDataset(dataset=opt.dataset_name, image_set="test")
 test_dl = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.batch_size, shuffle=False)
 
-for batch in test_dl:
-    # Model inputs
-    xyz_input = batch["A"].cuda()
-    out_imgs = model_g(xyz_input)
-    label = out_imgs.cpu().data
+image_writing_path = "generated_images"
+if not os.path.exists(image_writing_path):
+    os.makedirs(image_writing_path)
+
+for i, batch in enumerate(test_dl, start=1):
+    pass
+    # # Model inputs
+    # xyz_input = batch["A"].cuda()
+    # out_imgs = model_g(xyz_input)
+    # label = out_imgs.cpu().data
+
+    # for img_num in range(opt.batch_size):
+    #     print(f"Saving image number {i}_{img_num} out of {len(test_dl)}")
+    #     np.save(f"{image_writing_path}/gen_imgs_{i}_{img_num}.npy", label.numpy().astype(np.float32)[img_num])  # typing: ignore
 
     # get Visual Saliency-induced Index (VSI)
     # vsi_score = vsi(label.type(Tensor), batch["B"].type(Tensor))
     # print(vsi_score)
 
     # XYZ (only Z)
-    show_xyz(batch["A"].numpy(), cols=2)
+    # show_xyz(batch["A"].numpy(), cols=2)
     
     # ground truth RGB
-    show_rgb(batch["B"].numpy(), cols=2)
+    # show_rgb(batch["B"].numpy(), cols=2)
 
     # predicted RGB
-    show_rgb(label.numpy(), cols=2)
+    # show_rgb(label.numpy(), cols=2)
 
     # difference
-    show_diff(batch["B"].numpy(), label.numpy(), cols=2)
+    # show_diff(batch["B"].numpy(), label.numpy(), cols=2)
+
+data_base_dir = os.environ.get("DATASET_ROOT") or "."
+dataset_path = os.path.join(data_base_dir, "melbourne-top")
+rgb_path = os.path.join(dataset_path, "test", "rgb_data")
+
+print("computing fid ...")
+score = fid.compute_fid(
+    fdir1=rgb_path,
+    fdir2=image_writing_path,
+    mode="clean",
+    num_workers=4,
+)
+
+print(score)
