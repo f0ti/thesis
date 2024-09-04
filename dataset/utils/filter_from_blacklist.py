8515c6b8dda65426ad83233fc8e4e5d1c1527ef1
fotikerkeshi <foti.kerkeshi@hotmail.com>
2024-09-04 18:08:49 +0200

    remove images from blacklist

--- dataset/utils/filter_from_blacklist.py
+++ dataset/utils/filter_from_blacklist.py
@@ -1,16 +1,20 @@
 import os
 import sys
 
-def filter_from_blacklist(blacklist_name: str = "blacklist_0.97.txt", dataset_name: str = "melbourne-top"):
+def filter_from_blacklist(blacklist_name: str, dataset_name: str):
     with open(blacklist_name, "r") as f:
         blacklist = f.readlines()
         blacklist = [line.strip() for line in blacklist]
 
+    print("Removing images from blacklist: ", len(blacklist))
     for line in blacklist:
-        os.remove(f"../{dataset_name}/rgb_data/{line}")
-        os.remove(f"../{dataset_name}/xyz_data/{line}")
+        try:
+            os.remove(f"../{dataset_name}/rgb_data/{line}")
+            os.remove(f"../{dataset_name}/xyz_data/{line}")
+        except FileNotFoundError:
+            print(f"File not found: {line}")
 
 if __name__ == "__main__":
     blacklist_name = sys.argv[1]
     dataset_name = sys.argv[2]
-    filter_from_blacklist(blacklist_name)
+    filter_from_blacklist(blacklist_name, dataset_name)
